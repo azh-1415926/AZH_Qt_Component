e5fad4fc80f6577cb428dd4a62576bb439deb9eb
azh-1415926 <AzhandHJ@outlook.com>
2023-09-30 09:33:13 +0800

    将 src 文件夹改为 test，用于测试 component 组件

--- test/TestForQtComponent.cpp
+++ test/TestForQtComponent.cpp
@@ -1,8 +1,13 @@
 #include "TestForQtComponent.h"
 
+#include <settingFile.h>
+
 TestForQtComponent::TestForQtComponent(QWidget* parent)
     : QMainWindow(parent)
 {
+    settingFile setting;
+    setting.add("test",123);
+    setting.save("test.json");
 }
 
 TestForQtComponent::~TestForQtComponent()
