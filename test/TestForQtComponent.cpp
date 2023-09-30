cd27c24e30b9b0b1497eb49c224f3ccdfbe4b49c
azh-1415926 <AzhandHJ@outlook.com>
2023-09-30 21:51:40 +0800

    添加之前写答题应用的组件

--- test/TestForQtComponent.cpp
+++ test/TestForQtComponent.cpp
@@ -1,15 +1,30 @@
 #include "TestForQtComponent.h"
 
 #include <settingFile.h>
+#include <calcMD5.hpp>
+#include <collectbutton.h>
+#include <clickoptions.h>
 
 TestForQtComponent::TestForQtComponent(QWidget* parent)
     : QMainWindow(parent)
 {
-    settingFile setting;
-    setting.add("test",123);
-    setting.save("test.json");
+    this->resize(600,800);
+    collectButton* btn=new collectButton(this);
+    clickOptions* option=new clickOptions(this);
+    labelOfTest1=new QLabel("label",this);
+    labelOfTest1->setGeometry(100,0,50,50);
+    option->setGeometry(200,0,200,400);
+    connect(btn,&collectButton::collected,this,[&]()
+    {
+        labelOfTest1->setText("已收藏");
+    });
+    connect(btn,&collectButton::uncollected,this,[&]()
+    {
+        labelOfTest1->setText("取消收藏");
+    });
 }
 
 TestForQtComponent::~TestForQtComponent()
 {
+    delete labelOfTest1;
 }
--- test/TestForQtComponent.h
+++ test/TestForQtComponent.h
@@ -1,10 +1,17 @@
 #pragma once
 #include <QMainWindow>
+#include <QLabel>
 
 class TestForQtComponent : public QMainWindow {
     Q_OBJECT
 
+private:
+    QLabel* labelOfTest1;
+
 public:
     TestForQtComponent(QWidget* parent = nullptr);
     ~TestForQtComponent();
+
+private:
+    void initalTestForQtComponent();
 };
